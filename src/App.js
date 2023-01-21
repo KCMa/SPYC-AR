92f6280a6aa2d5f0f167aaf7c2cf5797e8627409
KCMa <83592482+KCMa@users.noreply.github.com>
2023-01-21 21:41:54 +0800

    takephoto testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React, { useState } from 'react';
-import 'mind-ar/dist/mindar-image.prod.js';
-import 'aframe';
-import 'mind-ar/dist/mindar-image-aframe.prod.js';
-import MindARViewer from './mindar-viewer';
+import React, { useState } from "react";
+import "mind-ar/dist/mindar-image.prod.js";
+import "aframe";
+import "mind-ar/dist/mindar-image-aframe.prod.js";
+import MindARViewer from "./mindar-viewer";
+import $ from "jquery";
+
+// const { JSDOM } = require("jsdom");
+// const { window } = new JSDOM("");
+// const $ = require("jquery")(window);
 
 function App() {
   const [started, setStarted] = useState(false);
+  const takePicture = () => {
+    document.querySelector("video").pause();
+
+    const video = document.getElementsByTagName("video")[0];
+
+    const canvas = document.createElement("canvas");
+
+    var window_width = $(window).outerWidth();
+    var window_height = $(window).outerHeight();
+
+    var v_width = $(video).outerWidth();
+    var v_height = $(video).outerHeight();
+
+    var a_width =
+      document.querySelector("a-scene").components.screenshot.data.width * 0.5;
+    var a_height =
+      document.querySelector("a-scene").components.screenshot.data.height * 0.5;
+    canvas.width = $(window).outerWidth();
+    canvas.height = $(window).outerHeight();
+
+    canvas
+      .getContext("2d")
+      .drawImage(
+        video,
+        0,
+        parseFloat($("video").css("top")),
+        v_width,
+        v_height
+      );
+
+    var imgData = document
+      .querySelector("a-scene")
+      .components.screenshot.getCanvas("perspective");
+
+    canvas
+      .getContext("2d")
+      .drawImage(imgData, 0, 0, window_width, window_height);
+
+    if (window.navigator.msSaveOrOpenBlob) {
+      var blobObject = canvas.msToBlob();
+      window.navigator.msSaveOrOpenBlob(blobObject, "download.png");
+    } else {
+      var a = document.createElement("a");
+      a.href = canvas.toDataURL("image/jpeg");
+      console.log(a.href);
+      a.download = "download.jpeg";
+      a.click();
+    }
+    document.querySelector("video").play();
+  };
+
   return (
     <div className="App">
-      <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
+      <h1>
+        Example React component with{" "}
+        <a href="https://github.com/hiukim/mind-ar-js" target="_blank">
+          MindAR
+        </a>
+      </h1>
 
       <div>
-	{!started && <button onClick={() => {setStarted(true)}}>Start</button>}
-	{started && <button onClick={() => {setStarted(false)}}>Stop</button>}
+        {!started && (
+          <button
+            onClick={() => {
+              setStarted(true);
+            }}
+          >
+            Start
+          </button>
+        )}
+        {started && (
+          <button
+            onClick={() => {
+              setStarted(false);
+            }}
+          >
+            Stop
+          </button>
+        )}
       </div>
 
       {started && (
-	<div className="container">
-	  <MindARViewer/>
-	  <video></video>
-	</div>
+        <div className="container">
+          <MindARViewer />
+          <video></video>
+        </div>
       )}
+      <button onClick={takePicture}>Take Picture</button>
     </div>
   );
 }
