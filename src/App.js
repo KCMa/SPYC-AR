1a74fb7db60f940f42f5030ac1142651fa6525dd
KCMa <83592482+KCMa@users.noreply.github.com>
2023-01-21 19:49:30 +0800

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import 'mind-ar/dist/mindar-image.prod.js';
 import 'aframe';
 import 'mind-ar/dist/mindar-image-aframe.prod.js';
-import './App.css';
 import MindARViewer from './mindar-viewer';
 
 function App() {
