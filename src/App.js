70718fba6fdc9a4ea777ec89ec4aa29817e4c42e
KCMa <83592482+KCMa@users.noreply.github.com>
2023-03-21 17:33:20 +0800

    123

--- src/App.js
+++ src/App.js
@@ -62,15 +62,73 @@ function App() {
     document.querySelector("video").play();
   };
 
+
+
+
+  const takeScreenshot = () => {
+    document
+      .querySelector("a-scene")
+      .components.screenshot.capture("perspective");
+  };
+
+
+
   return (
     <div className="App">
       <h1>
         SPYC 45th AR
       </h1>
 
-      <div>
-        {!started && (
-          <button
+
+ 
+
+      <a-scene physics style={{zIndex: 0}}>
+        <a-box
+          position="-1 4 -3"
+          rotation="0 45 0"
+          color="#4CC3D9"
+          dynamic-body
+        ></a-box>
+
+
+        <a-plane
+          position="0 0 -4"
+          rotation="-90 0 0"
+          width="4"
+          height="4"
+          color="#7BC8A4"
+          static-body
+        ></a-plane>
+        <a-sky color="#ECECEC"></a-sky>
+
+        <a-assets>
+          <a-asset-item
+            id="tree"
+            src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/softmind/scene.gltf"
+          ></a-asset-item>
+        </a-assets>
+
+        {/* <!-- Using the asset management system. --> */}
+        <a-gltf-model
+          src="#tree"
+          position="0 2 -5"
+          rotation="0 0 0"
+          scale="0.02 0.02 0.02"
+        ></a-gltf-model>
+
+      </a-scene>
+      <h1>Welcome to SPYC AR App</h1>
+      <button
+        style={{ "z-index": 9999, position: "absolute" }}
+        onClick={takeScreenshot}
+      >
+        Take a screenshot
+      </button>
+
+
+      {!started && (
+        <button
+        style={{ "z-index": 9999, position: "absolute" }}
             onClick={() => {
               setStarted(true);
             }}
@@ -80,6 +138,8 @@ function App() {
         )}
         {started && (
           <button
+        style={{ "z-index": 9999, position: "absolute" }}
+
             onClick={() => {
               setStarted(false);
             }}
@@ -87,7 +147,6 @@ function App() {
             Stop
           </button>
         )}
-      </div>
 
       {started && (
         <div className="container">
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ScreenCaptureFunction from './screenCapture';
 import WelcomePage from './WelcomePage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <WelcomePage />
+    <ScreenCaptureFunction />
   </React.StrictMode>,
   document.getElementById('root')
 );
